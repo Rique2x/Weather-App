f55a6f6618691ada84ca5ed2255d0c6b8f706710
Rique2 <enrique2heinrich@gmail.com>
2023-03-21 11:02:05 +0200

    export weather function into the weather.js file (js)

---
+++ src/Weather.js
@@ -0,0 +1,17 @@
+async function getWeather(place) {
+	const resp = await fetch(
+		`https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=e3cd04868ae1a93483419e83e5d735b7`
+	);
+	console.log(resp);
+
+	const respData = await resp.json();
+	console.log(respData);
+
+	const weatherData = respData.main;
+	console.log(weatherData);
+
+    const weatherSubData = respData.weather[0];
+	console.log(weatherSubData);
+}
+
+export default getWeather;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-async function getWeather(place) {
-	const resp = await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=e3cd04868ae1a93483419e83e5d735b7`
-	);
-	console.log(resp);
+import Weather from "./Weather";
 
-	const respData = await resp.json();
-	console.log(respData);
-
-	const weatherData = respData.main;
-	console.log(weatherData);
-}
-
-getWeather("Jamshedpur");
+console.log("webpack");
